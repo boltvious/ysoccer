ef383a4a3b9fff26d57c4fdb2715be9b99354223
boltvious <54421217+boltvious@users.noreply.github.com>
2025-08-25 03:16:37 +0700

    Add files via upload

---
+++ database/ImportConfig.java
@@ -0,0 +1,15 @@
+/*   */ package com.ygames.ysoccer.database;
+/*   */ 
+/*   */ import java.util.ArrayList;
+/*   */ import java.util.List;
+/*   */ 
+/*   */ public class ImportConfig
+/*   */ {
+/* 8 */   public List<ImportFileConfig> files = new ArrayList<>();
+/*   */ }
+
+
+/* Location:              C:\Users\Master\Downloads\ysoccer19\ysoccer.jar!\com\ygames\ysoccer\database\ImportConfig.class
+ * Java compiler version: 7 (51.0)
+ * JD-Core Version:       1.1.3
+ */
---
+++ database/ImportFileConfig.java
@@ -0,0 +1,21 @@
+package com.ygames.ysoccer.database;
+
+import com.ygames.ysoccer.match.Team;
+
+public class ImportFileConfig {
+  public String filename;
+  
+  public Team.Type type;
+  
+  public String country;
+  
+  public String[] leagues;
+  
+  public String continent;
+}
+
+
+/* Location:              C:\Users\Master\Downloads\ysoccer19\ysoccer.jar!\com\ygames\ysoccer\database\ImportFileConfig.class
+ * Java compiler version: 7 (51.0)
+ * JD-Core Version:       1.1.3
+ */
